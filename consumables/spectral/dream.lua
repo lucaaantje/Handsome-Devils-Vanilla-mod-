3b3c5ed98befc068d4387243c23177f20e9e9e8b
lucaaantje <128933254+lucaaantje@users.noreply.github.com>
2025-03-27 23:28:40 +0100

    More balanced dream tag
     Now gives 1 negative tag and 4 random tags instead of 10 random tags, cost increased to 6

--- consumables/spectral/dream.lua
+++ consumables/spectral/dream.lua
@@ -1,9 +1,16 @@
-SMODS.Consumable {
+SMODS.Consumable({
 	key = 'dream',
 	set = 'Spectral',
 	config = {
 		extra = {
-			max_tags = 10
+			max_tags = 4
+		}
+	},
+	loc_txt = {
+		name = "Dream Tag",
+		text = {
+			"Adds a {C:dark_edition}negative{} tag",
+			"followed by {C:attention}#1#{} random tags"
 		}
 	},
 	loc_vars = function(self, info_queue, card)
@@ -14,26 +21,32 @@ SMODS.Consumable {
 	soul_set = "Joker",
 	atlas = 'Consumables',
 	pos = { x = 1, y = 1 },
-	cost = 4,
+	cost = 6,
 	use = function(self, card, context, copier)
 		local used_consumable = copier or card
 		local tags_to_make = card.ability.extra.max_tags
+		
+		-- Add negative tag first
 		G.E_MANAGER:add_event(Event({
 			trigger = 'before',
-			delay = 0.75,
+			delay = 0.25,
 			func = function()
-				--empty event for timing purposes
+				used_consumable:juice_up()
+				play_sound('generic1', 0.9 + math.random() * 0.1, 0.8)
+				add_tag(Tag('tag_negative'))
 				return true
 			end
 		}))
+
+		-- Immediately add random tags
 		for i = 1, tags_to_make do
 			G.E_MANAGER:add_event(Event({
 				trigger = 'before',
-				delay = 0.5,
+				delay = 0.25 + (i * 0.1),
 				func = function()
 					used_consumable:juice_up()
 					play_sound('generic1', 0.9 + math.random() * 0.1, 0.8)
-					add_tag(HNDS.poll_tag("dream_",  HNDS.get_shop_joker_tags()))
+					add_tag(HNDS.poll_tag("dream_", HNDS.get_shop_joker_tags()))
 					return true
 				end
 			}))
@@ -41,5 +54,5 @@ SMODS.Consumable {
 	end,
 	can_use = function(self, card)
 		return true
-	end,
-}
+	end
+})
